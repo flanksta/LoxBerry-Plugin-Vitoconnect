a53600d104f070a4c7fa74ed1a79bbad59fd4b8b
flanksta <153260194+flanksta@users.noreply.github.com>
2025-12-17 11:32:53 +0100

    Update defines.php
    shortened api URL for version v2

--- webfrontend/htmlauth/defines.php
+++ webfrontend/htmlauth/defines.php
@@ -8,8 +8,7 @@ define ("INSTALLDATA", TMPPREFIX . "installdata.json");
 define ("client_secret", "2e21faa1-db2c-4d0b-a10f-575fd372bc8c-575fd372bc8c");
 define ("callback_uri", "http://localhost:4200/");
 //define ("apiURL", "https://api.viessmann-climatesolutions.com/iot/v1/equipment/");
-define ("apiURLv2", "https://api.viessmann-climatesolutions.com/iot/v2/features/");
-define ("apiURLv2_base", "https://api.viessmann-climatesolutions.com/iot/v2/");
+define ("apiURLv2", "https://api.viessmann-climatesolutions.com/iot/v2/");
 define ("authorize_URL", "https://iam.viessmann-climatesolutions.com/idp/v3/authorize");
 define ("token_url", "https://iam.viessmann-climatesolutions.com/idp/v3/token");
 
