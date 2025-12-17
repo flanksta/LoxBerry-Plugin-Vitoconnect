6582ce31701ee5487c87ac100285b4c355922dcc
flanksta <153260194+flanksta@users.noreply.github.com>
2025-12-17 11:08:23 +0100

    Update defines.php
    changes base URL of API version 2 and commented apiversion v1 because it is totally deprecated

--- webfrontend/htmlauth/defines.php
+++ webfrontend/htmlauth/defines.php
@@ -6,12 +6,12 @@ define ("LOGINFILE", TMPPREFIX . "sessiondata.json");
 define ("INSTALLDATA", TMPPREFIX . "installdata.json");
 
 define ("client_secret", "2e21faa1-db2c-4d0b-a10f-575fd372bc8c-575fd372bc8c");
-define ("callback_uri", "http://localhost:4200/"); 
-define ("apiURLBase", "https://api.viessmann-platform.io/iot/v1/equipment/");
-define ("apiURL", "https://api.viessmann.com/iot/v1/equipment/");
-define ("apiURLv2", "https://api.viessmann.com/iot/v2/features/");
-define ("authorize_URL", "https://iam.viessmann.com/idp/v2/authorize");
-define ("token_url", "https://iam.viessmann.com/idp/v2/token");
+define ("callback_uri", "http://localhost:4200/");
+//define ("apiURL", "https://api.viessmann-climatesolutions.com/iot/v1/equipment/");
+define ("apiURLv2", "https://api.viessmann-climatesolutions.com/iot/v2/features/");
+define ("apiURLv2_base", "https://api.viessmann-climatesolutions.com/iot/v2/");
+define ("authorize_URL", "https://iam.viessmann-climatesolutions.com/idp/v3/authorize");
+define ("token_url", "https://iam.viessmann-climatesolutions.com/idp/v3/token");
 
 // The Navigation Bar
 $navbar[1]['Name'] = "Settings";
@@ -21,4 +21,4 @@ $navbar[2]['Name'] = "Query links and data";
 $navbar[2]['URL'] = 'showdata.php';
 
 $navbar[3]['Name'] = "Log";
-$navbar[3]['URL'] = 'log.php'; 
+$navbar[3]['URL'] = 'log.php';
